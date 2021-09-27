5ddb50319d2d139df0b1976d84b16cc357822aea
Colin Walker <colin@colinwalker.blog>
2021-09-27 14:51:32 +0100

    Update footer.php
    Changed Licence

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         </span>
         <br/>
         <span class="licSpan">
-            <?php if(getOption('Use_Now') == 'yes') { ?><a href="/now/">NOW</a> | <?php } ?><a href="/colophon/">COLOPHON</a><?php if($copyright != "yes") { ?> | Content: <a href="https://creativecommons.org/licenses/by-nc/2.0/uk/">CC BY-NC 2.0 UK</a><?php } ?>
+            <?php if(getOption('Use_Now') == 'yes') { ?><a href="/now/">NOW</a> | <?php } ?><a href="/colophon/">COLOPHON</a><?php if($copyright != "yes") { ?> | Content: <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a><?php } ?>
         </span>
     </div>
 
